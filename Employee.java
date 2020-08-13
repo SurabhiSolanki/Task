65a7dadc2b92420cdddc03f1cb55dab35f1152de
SurabhiSolanki <69638912+SurabhiSolanki@users.noreply.github.com>
2020-08-13 11:24:39 -0400

    I added equals and hashcode method for comparison

--- Employee.java
+++ Employee.java
@@ -22,6 +22,25 @@ class Employee{
 		return "Employee [id=" + id + ", name=" + name + "]";
 	}
 	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
 }
 
 
